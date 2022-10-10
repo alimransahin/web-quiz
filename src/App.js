0c7930163ad515ea84295085701e3f9e0738035e
alimransahin <92633905+alimransahin@users.noreply.github.com>
2022-10-10 23:24:55 +0600

    data load successfull

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Main from './Layout/Main';
 import Home from './Components/Home/Home';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 function App() {
-  const router=createBrowserRouter[
-    {
-      path:'/',
-      element:<Main></Main>,
-      children:[
-        {
-          path:'/',
-          element:<Home></Home>,
-          loader: async () => fetch('https://openapi.programming-hero.com/api/quiz')
-        }
-      ]
-    }
-  ]
+  const router=createBrowserRouter([
+   {
+    path:'/',
+    element:<Main></Main>,
+    children:[
+      {
+        path:'/',
+        element:<Home></Home>,
+        loader: async () => fetch('https://openapi.programming-hero.com/api/quiz')
+      }
+    ]
+   }
+  ])
   return (
     <div>
       <RouterProvider router={router}></RouterProvider>
+      {/* <RouterProvider router={router}></RouterProvider> */}
     </div>
   );
 }
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
-    const data=useLoaderData();
+    const {data}=useLoaderData();
     return (
         <div>
             <h2>This is Home {data.length}</h2>
